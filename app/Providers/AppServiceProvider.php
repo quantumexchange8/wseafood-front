5cf1bc840e157457859acbfdfda26fcb0b1989f9
quantumexchange8 <96815929+quantumexchange8@users.noreply.github.com>
2025-07-03 23:19:48 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (App::environment('production') || App::environment('staging')) {
+            resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
