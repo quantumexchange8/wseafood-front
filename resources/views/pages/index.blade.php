e7f9e6d86dfe088a7f50b10dbf42db5b23b229b1
unknown <demondrake00@gmail.com>
2025-07-04 17:03:22 +0800

    modify text alignment in landing page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -14,22 +14,21 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-7">
-                    <div class="hero-text">
-                    <h2 style="line-height: 1.5;">
-                        <!-- <span style="display: block;">🧡</span> -->
-                        <span style="display: block;">Your Table, Your Rewards</span>
-                    </h2>
+                        <div class="hero-text">
+                            <h2 style="line-height: 1.5;">
+                                <span style="display: block;">Your Table, Your Rewards</span>
+                            </h2>
 
-                    <p style="display: flex; align-items: start; gap: 0.5rem; line-height: 2;">
-                        <span style="flex-shrink: 0;">✨</span>
-                        <span>Earn points every time you dine with us at Wondering Plan A Sdn Bhd!</span>
-                    </p>
+                            <p style="display: flex; align-items: start; gap: 0.5rem; line-height: 2;">
+                                <span style="flex-shrink: 0;">✨</span>
+                                <span>Earn points every time you dine with us at Wondering Plan A Sdn Bhd!</span>
+                            </p>
 
-                    <div class="hero-btn-area">
-                        <a href="#works" class="hero-btn">How it Work</a>
-                        <a href="#download" class="hero-btn btn-active">Get Now</a>
-                    </div>
-                </div>
+                            <div class="hero-btn-area">
+                                <a href="#works" class="hero-btn">How it Work</a>
+                                <a href="#download" class="hero-btn btn-active">Get Now</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -48,23 +47,28 @@
     <!--service area start -->
     <div id="features" class="section-padding">
         <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="section-title">
-                        <h2>Member Perks</h2>
-                    </div>
-                    <div class="col-md-6">
+            <div class="section-title">
+                <h2>Member Perks</h2>
+            </div>
+            <div class="single-service-area">
+                <div class="row single-service-row">
+                    <div class="col-md-5 col-sm-6">
                         <div class="single-service">
                             <img src="assets/img/icon/exclusive_promo.png" alt="">
-                            <h2>Exclusive promos</h2>
-                            <p style="text-align: start;">Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.</p>
+                            <div>
+                                <h2>Exclusive promos</h2>
+                                <p>Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.</p>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-md-6">
+
+                    <div class="col-md-5 col-sm-6">
                         <div class="single-service">
                             <img src="assets/img/icon/fast_rewards.png" alt="">
-                            <h2>Fast rewards</h2>
-                            <p style="text-align: start;">Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.</p>
+                            <div>
+                                <h2>Fast rewards</h2>
+                                <p>Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -118,7 +122,7 @@
                             <img src="assets/img/icon/register.png" alt="" style="height: 62px; width: auto; margin: 0 auto;">
                             <div style="flex-grow: 1; display: flex; flex-direction: column; justify-content: flex-start;">
                                 <h2>Register for free</h2>
-                                <p style="text-align: start; align-items: center;">Lorem ipsum dolor sit ameteped consecteadop adipisicing elitab sed eiusmod temporara incident</p>
+                                <p>Lorem ipsum dolor sit ameteped consecteadop adipisicing elitab sed eiusmod temporara incident</p>
                             </div>
                         </div>
                     </div>
@@ -128,7 +132,7 @@
                             <img src="assets/img/icon/earn_points.png" alt="" style="height: 62px; width: auto; margin: 0 auto;">
                             <div style="flex-grow: 1; display: flex; flex-direction: column; justify-content: flex-start;">
                                 <h2>Earn points</h2>
-                                <p style="text-align: start; align-items: center;">Lorem ipsum dolor sit ameteped consecteadop adipisicing elitab sed eiusmod temporara incident</p>
+                                <p>Lorem ipsum dolor sit ameteped consecteadop adipisicing elitab sed eiusmod temporara incident</p>
                             </div>
                         </div>
                     </div>
@@ -138,7 +142,7 @@
                             <img src="assets/img/icon/redeem_points.png" alt="" style="height: 62px; width: auto; margin: 0 auto;">
                             <div style="flex-grow: 1; display: flex; flex-direction: column; justify-content: flex-start;">
                                 <h2>Redeem points</h2>
-                                <p style="text-align: start; align-items: center;">Lorem ipsum dolor sit ameteped consecteadop adipisicing elitab sed eiusmod temporara incident</p>
+                                <p>Lorem ipsum dolor sit ameteped consecteadop adipisicing elitab sed eiusmod temporara incident</p>
                             </div>
                         </div>
                     </div>
@@ -165,7 +169,7 @@
             </div>
             <div class="row">
                 <div class="col-md-4">
-                    <div class="feature-area">
+                    <div class="feature-area text-center">
                         <div class="feature">
                             <div class="feature-icon">
                                 <img src="assets/img/icon/feature-icon-1.png" alt="">
@@ -195,7 +199,7 @@
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <div class="feature-area">
+                    <div class="feature-area text-center">
                         <div class="feature">
                             <div class="feature-icon">
                                 <img src="assets/img/icon/feature-icon-4.png" alt="">
@@ -250,116 +254,4 @@
         </div>
     </div>
     <!-- download app section end-->
-    <!--terms and condition area start -->
-    <!-- <div id="terms_and_conditions" class="page-section-padding gray-bg">
-        <div class="container">
-            <div class="row">
-                <div class="col-md">
-                    <div class="section-title">
-                        <h2>Terms & Conditions / 服务条款</h2>
-                        <h5>Welcome to Wondering Plan A Sdn Bhd Membership App (“the App”). By using this App, you agree to be bound by the following Terms & Conditions.</h5>
-                        <h5>欢迎使用 Wondering Plan A Sdn Bhd的会员应用程序(“本应用”)。使用本应用即表示您同意以下服务条款。</h5>
-                        <p>
-                            <h4>1. Membership Eligibility / 会员资格</h4>
-                            <div>You must be at least 18 years old to register. Membership is non-transferable.</div>
-                            <div>您必须年满18岁方可注册。会员资格不得转让。</div>
-                        </p>
-
-                        <p>
-                            <h4>2. Points Collection & Redemption / 积分累积与兑换</h4>
-                            <div>Points are awarded based on in-store purchases only and have no cash value. Expired points will not be reinstated.</div>
-                            <div>积分仅限于店内消费后获得，不具现金价值。过期积分将不会恢复。</div>
-                        </p>
-
-                        <p>
-                            <h4>3. Modification & Termination / 修改与终止</h4>
-                            <div>Wondering Plan A Sdn Bhd reserves the right to change or terminate the program at any time without prior notice.</div>
-                            <div>Wondering Plan A Sdn Bhd有权在不另行通知的情况下，修改或终止会员计划。</div>
-                        </p>
-                        
-                        <p>
-                            <h4>4. Misuse of Account / 账户滥用</h4>
-                            <div>Accounts found to be abusing the system (e.g. fraud) will be suspended without compensation.</div>
-                            <div>如发现账户存在滥用行为（例如欺诈），我们有权暂停其使用，且不予赔偿。</div>
-                        </p>
-
-                        <p>
-                            <h4>5. Governing Law / 适用法律</h4>
-                            <div>These Terms shall be governed by the laws of Malaysia.</div>
-                            <div>本条款受马来西亚法律管辖。</div>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
-    <!--terms and condition area end -->
-    <!--privacy policy start -->
-    <!-- <div id="privacy_policy" class="page-section-padding">
-        <div class="container">
-            <div class="row">
-                <div class="col-md">
-                    <div class="section-title">
-                        <h2>Privacy Policy / 隐私政策</h2>
-                        <h5>This Privacy Policy explains how Wondering Plan A Sdn Bhd (“we”, “us”, “our”) collects, uses, and protects your information when you use our App.</h5>
-                        <h5>本隐私政策说明 Wondering Plan A Sdn Bhd(“我们”)如何在您使用本应用程序时收集、使用及保护您的信息。</h5>
-                        <p>
-                            <h4>1. Information We Collect / 我们收集的信息</h4>
-                            <div>We may collect your name, phone number, email address, date of birth, and visit or transaction records.</div>
-                            <div>我们可能收集您的姓名、电话号码、电邮地址、出生日期，以及消费和访问记录。</div>
-                        </p>
-
-                        <p>
-                            <h4>2. How We Use It / 使用方式</h4>
-                            <ul>
-                                <h4>We use your information to:</h4>
-                                <li>Manage your membership and points</li>
-                                <li>Send promotional messages (with your consent)</li>
-                                <li>Improve customer experience</li>
-                            </ul>
-                        </p>
-                        <p>
-                            <ul>
-                                <h4>我们使用您的信息以：</h4>
-                                <li>管理会员和积分</li>
-                                <li>发送促销信息（在您同意的前提下</li>
-                                <li>提升顾客体验</li>
-                            </ul>
-                        </p>
-
-                        <p>
-                            <h4>3. Data Protection / 数据保护</h4>
-                            <div>We implement reasonable technical and organizational measures to protect your data against unauthorized access, alteration, disclosure, or destruction.</div>
-                            <div>我们实施合理的技术和组织措施来保护您的数据免遭未经授权的访问、更改、泄露或破坏。</div>
-                        </p>
-                        
-                        <p>
-                            <h4>4. Third Parties / 第三方共享</h4>
-                            <div>We do not sell or rent your data. Your data may be shared with service providers strictly for operating our app.</div>
-                            <div>我们不会出售或出租您的资料。我们可能与技术服务供应商共享资料，仅限用于本应用的运行。</div>
-                        </p>
-
-                        <p>
-                            <h4>5. Your Rights / 您的权利</h4>
-                            <div>You may request to view, correct, or delete your personal information by contacting us at: support@wonderingmember.com</div>
-                            <div>您可随时联系我们查阅、更正或删除您的个人信息: support@wonderingmember.com。</div>
-                        </p>
-
-                        <p>
-                            <h4>6. Cookies and Tracking/ Cookie 和追踪</h4>
-                            <div>Our Service may use cookies and similar technologies to enhance functionality and analyze usage.</div>
-                            <div>我们的服务可能会使用 Cookie 和类似技术来增强功能并分析使用情况。</div>
-                        </p>
-
-                        <p>
-                            <h4>7. Updates / 政策更新</h4>
-                            <div>We may update this Privacy Policy. Continued use of the App indicates acceptance of any changes.</div>
-                            <div>我们可能会更新本政策。继续使用本应用即表示您接受修改后的政策。</div>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
-    <!--privacy policy end -->
 </x-app-layout>
